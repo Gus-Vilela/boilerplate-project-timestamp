d74a4f2728a9093c0f5ab8ae316409f8bf995fe9
Gustavo Vilela <87400905+Gus-Vilela@users.noreply.github.com>
2023-05-17 15:48:46 +0000

    Update index.js, add date API

--- index.js
+++ index.js
@@ -2,31 +2,46 @@
 // where your node app starts
 
 // init project
-var express = require('express');
+var express = require("express");
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
-var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC
+var cors = require("cors");
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/:word?", function (req, res) {
+  if (/\d{13}/.test(req.params.word)) {
+    let date = new Date(Number(req.params.word));
+    res.json({
+      unix: Number(req.params.word),
+      utc: date.toUTCString(),
+    });
+  } else {
+    let date;
+    typeof req.params.word === "undefined"
+      ? (date = new Date())
+      : (date = new Date(req.params.word));
+    if (date.toString() === "Invalid Date") {
+      res.json({ error: date.toString() });
+    } else {
+      res.json({
+        unix: date.valueOf(),
+        utc: date.toUTCString(),
+      });
+    }
+  }
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log("Your app is listening on port " + listener.address().port);
 });
